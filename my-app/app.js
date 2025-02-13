e78d8145acc572702ec6587ffcba89786f1dfdcf
tanay123456789 <tanaydwivedi456@gmail.com>
2025-02-13 09:06:20 +0530

    fix:request body parser

---
+++ my-app/app.js
@@ -0,0 +1,34 @@
+const express=require("express");
+const app=express();
+
+app.use(express.json());
+
+let tasks=[];
+
+app.get("/tasks",(req,res)=>{
+
+    res.setHeader("Content-Type","application/json");
+    res.json({tasks})
+});
+
+app.post("/tasks",(req,res)=>{
+    const task=req.body.task;
+    if(!task){
+        return res.status(400).json({error:"Task is required"});
+    }
+    tasks.push(task);
+    res.status(201).send("task added");
+    
+});
+
+
+app.listen(3001,()=>{
+    console.log("Server is running on port 3001");
+});
+
+
+
+
+
+
+
