95c36d67a2e9acbd96b041cecefd88fea49a5c2f
tanay123456789 <tanaydwivedi456@gmail.com>
2025-02-19 09:16:24 +0530

    feat:added update feature

--- my-app/app.js
+++ my-app/app.js
@@ -13,13 +13,27 @@ app.get("/tasks",(req,res)=>{
 
 app.post("/tasks",(req,res)=>{
     console.log("Request Body",req.body);
-    const task=req.body.task;
+    const task=req.body;
     if(!task){
         return res.status(400).json({error:"Task is required"});
     }
     tasks.push(task);
     res.status(201).send("task added");
     
+});
+
+app.put("/tasks",(req,res)=>{
+    const {index,newTask}=req.body;
+    if (index==null||newTask==null){
+        return res.status(400).json({ error: "Index and newTask are required" });
+    }
+    if(index<0||index>=tasks.length){
+        return res.status(400).json({error:"Invalid index"});
+    }
+
+    tasks[index]=newTask;
+    console.log("Request Body",req.body);
+    res.status(201).send("tasks updated");
 });
 
 
